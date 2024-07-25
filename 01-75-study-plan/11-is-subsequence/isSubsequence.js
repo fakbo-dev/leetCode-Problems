49ae1f8ece92685a324e71e25d0b971772c9b6d0
fakbo <fakbo0206@gmail.com>
2024-07-25 09:32:49 -0400

    starting

---
+++ 01-75-study-plan/11-is-subsequence/isSubsequence.js
@@ -0,0 +1,20 @@
+// Is Subsequence
+
+
+// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+// A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+// Steps
+
+//? 1. dados dos strings "s" y "t", devuelve "true" si s es una subsequence de t, de otro modo devuelve "false".
+
+//?  2. una subsecuencia de un string es un nuevo string formado desde el string original eliminando algunos (puede no ser ninguno) de los caracteres sin pertubar la posicion relativa de los caracteres restantes. (i.e, "ace" es una subsequencia de "abcde" mientras "aec" no lo es).
+
+
+
+
+const isSubsequence = (s, t) => {
+
+
+}
