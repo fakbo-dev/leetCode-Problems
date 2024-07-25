7c57067ee7e825feab673307d1dffdc9b2746f25
fakbo <fakbo0206@gmail.com>
2024-07-25 19:53:31 -0400

    works for now

--- 01-75-study-plan/11-is-subsequence/isSubsequence.js
+++ 01-75-study-plan/11-is-subsequence/isSubsequence.js
@@ -7,14 +7,41 @@
 
 // Steps
 
-//? 1. dados dos strings "s" y "t", devuelve "true" si s es una subsequence de t, de otro modo devuelve "false".
+//? 1. dados dos strings "s" y "t", devuelve "true" si s es una subsequencia de t, de otro modo devuelve "false".
 
 //?  2. una subsecuencia de un string es un nuevo string formado desde el string original eliminando algunos (puede no ser ninguno) de los caracteres sin pertubar la posicion relativa de los caracteres restantes. (i.e, "ace" es una subsequencia de "abcde" mientras "aec" no lo es).
 
 
 
-
 const isSubsequence = (s, t) => {
 
+    // Time complexity O(n)
+    // memory complexity O(1)
+    const [...leftString] = s;
+    const [...rightString] = t;
+    const n = rightString.length;
+    const newArr = [];
+    let p1 = 0;
+    let p2 = 0;
+
+    while (p1 < n) {
+
+        if (p1 < n && rightString[p1] === leftString[p2]) {
+
+            newArr.push(rightString[p1]);
+
+            p2++
+        }
+
+        p1++
+
+    }
+    const result = newArr.join("");
+    return result === s;
 
 }
+
+console.log(isSubsequence("abc", "ahbgdc")); // Ouput:  True
+
+
+
