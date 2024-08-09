7c4d607f5517fd0aabd31173de3e40fc2e6b846e
fakbo <fakbo0206@gmail.com>
2024-08-08 20:17:47 -0400

    you cannot beat us

---
+++ 09-slice window/01-maximu strong pair XOR I/maximumStrongPairXoR.ts
@@ -0,0 +1,21 @@
+// You are given a 0-indexed integer array nums. A pair of integers x and y is called a strong pair if it satisfies the condition:
+
+// |x - y| <= min(x, y)
+
+// You need to select two integers from nums such that they form a strong pair and their bitwise XOR is the maximum among all strong pairs in the array.
+
+// Return the maximum XOR value out of all possible strong pairs in the array nums.
+
+// Note that you can pick the same integer twice to form a pair.
+
+// TODO Steps
+
+// 1. 
+const maximumStrongPairXoR = (nums: number[]) => {
+
+
+};
+
+
+
+maximumStrongPairXoR([1, 2, 3, 4, 5]);
