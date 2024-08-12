33d22a5e4708a7272dc8fb224ecc3da9bab3e798
fakbo <fakbo0206@gmail.com>
2024-08-12 09:03:22 -0400

    restructure problem 1

--- 01-75-study-plan/01-merge-strings-alternately/mergeStringAlternately.js
+++ 01-75-study-plan/01-merge-strings-alternately/mergeStringAlternately.js
@@ -11,28 +11,20 @@
 // if one string is longaer than the other add the rest to the end of merge.
 
 const mergeAlternately = (word1, word2) => {
-    // Create the merge array
-    const merge = [];
-    // transform the string in to a array
-    const [...arr1] = word1;
-    const [...arr2] = word2;
+    const n = Math.max(word1.length, word2.length);
+    let result = "";
 
-    // Take the max length
-    const maxLength = Math.max(arr1.length, arr2.length);
-    // loop through the array
+    for (let i = 0; i < n; i++) {
 
-    for (let i = 0; i < maxLength; i++) {
-
-        if (i < arr1.length) {
-            merge.push(arr1[i]);
+        if (i < word1.length) {
+            result += `${word1[i]}`
         }
-
-        if (i < arr2.length) {
-            merge.push(arr2[i]);
+        if (i < word2.length) {
+            result += `${word2[i]}`
         }
     }
 
-    return merge.join("");
+    return result;
 }
 
 
@@ -40,3 +32,47 @@ const test1 = "abc";
 const test2 = "pqr";
 
 const result1 = mergeAlternately(test1, test2); //Expected Ouput = apbqcr;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mergeAlternately = (word1, word2) => {
+//     // Create the merge array
+//     const merge = [];
+//     // transform the string in to a array
+//     const [...arr1] = word1;
+//     const [...arr2] = word2;
+
+//     // Take the max length
+//     const maxLength = Math.max(arr1.length, arr2.length);
+//     // loop through the array
+
+//     for (let i = 0; i < maxLength; i++) {
+
+//         if (i < arr1.length) {
+//             merge.push(arr1[i]);
+//         }
+
+//         if (i < arr2.length) {
+//             merge.push(arr2[i]);
+//         }
+//     }
+
+//     return merge.join("");
+// }
